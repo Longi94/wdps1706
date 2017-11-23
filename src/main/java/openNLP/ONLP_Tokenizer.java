6fb73c9d49bde693cb8eeec578bcbc0a5ae75aea
Konrad Karaś <knrdkrs@gmail.com>
2017-11-23 19:05:40 +0100

    chenges tokenizer tool to SimpleTokenizer
    SimpleTokenizer tool gives better results than TokenizerME

--- src/main/java/openNLP/ONLP_Tokenizer.java
+++ src/main/java/openNLP/ONLP_Tokenizer.java
@@ -4,15 +4,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import opennlp.tools.tokenize.SimpleTokenizer;
+import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
 
 public class ONLP_Tokenizer {
 	
-	private static TokenizerME tokenizer = null;
-
+	// private static TokenizerME tokenizer = null;
+	private static Tokenizer tokenizer = null;
+	
 	private static void createInstance() {
-		InputStream modelIn = null;
+		/*InputStream modelIn = null;
 		try {
 		modelIn = new FileInputStream("models/en-token.bin");
 		TokenizerModel model = new TokenizerModel(modelIn);
@@ -29,7 +32,10 @@ public class ONLP_Tokenizer {
 				catch (IOException e) {
 				}
 			}
-		}
+		}*/
+		
+		//use SimpleTokenizer - better results
+		tokenizer = SimpleTokenizer.INSTANCE;
 	}
 	
 	public static String[] tokenize(String input) {
