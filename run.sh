9504d94462f01afa48382bec142280d4c718aa9e
Long Tran <lngtrn94@gmail.com>
2017-12-01 17:34:45 +0100

    output the result with the bash script

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ INFILE=${2:-"hdfs:///user/bbkruit/CC-MAIN-20160924173739-00000-ip-10-143-35-109.
 
 EXECUTORS=20
 
+timestamp=$(date +%s)
+
+{
 
 hdfs dfs -rm -r -f /user/wdps1706/spark-data
 
@@ -14,5 +17,28 @@ hdfs dfs -rm -r -f /user/wdps1706/spark-data
   --packages org.jsoup:jsoup:1.10.3,com.robbypond:boilerpipe:1.2.3,org.apache.opennlp:opennlp-uima:1.8.3,com.google.code.gson:gson:2.8.2 \
   --master yarn \
   --deploy-mode cluster \
-  --num-executors ${EXECUTORS}
+  --num-executors ${EXECUTORS} \
   wdps1706-1.0-SNAPSHOT.jar ${INFILE} ${ATT} ${EXECUTORS}
+
+
+PYSPARK_PYTHON=$(readlink -f $(which python)) ~/spark-2.1.2-bin-without-hadoop/bin/spark-submit \
+  --master yarn \
+  --deploy-mode cluster \
+  --num-executors ${EXECUTORS} \
+  spark_linker.py
+
+} &> "logs_${timestamp}"
+
+
+hdfs_ls=$(hdfs dfs -ls /user/wdps1706/spark-data/links)
+readarray -t files <<< "$hdfs_ls"
+
+regex="^.*(\/user\/wdps1706\/spark-data\/links\/part-[0-9][0-9][0-9][0-9][0-9])$"
+for line in "${files[@]}"
+do
+    if [[ ${line} =~ ${regex} ]]
+    then
+        path="${BASH_REMATCH[1]}"
+        hdfs dfs -cat ${path}
+    fi
+done
